11d6d3870ccea091fd3a538102cb5df3287a53a8
= <=>
2024-12-01 17:03:46 +0100

    clean up connection info to db

--- src/SaveMetadataToDb.py
+++ src/SaveMetadataToDb.py
@@ -1,15 +1,24 @@
 import psycopg2
 from typing import List, Dict
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 def store_articles_in_db(articles: List[Dict]) -> None:
-    # TODO: Add this to .env
+    dbname = os.getenv("DB_NAME", "postgres")
+    user = os.getenv("DB_USER", "postgres")
+    password = os.getenv("DB_PASSWORD", "password")
+    host = os.getenv("DB_HOST", "localhost")
+    port = os.getenv("DB_PORT", "5432")
+
+    # Connect to the database
     conn = psycopg2.connect(
-        dbname="postgres",
-        user="matthieuneau",
-        password="postgres",
-        host="localhost",
-        port="5432",
+        dbname=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port,
     )
     cursor = conn.cursor()
 
@@ -47,7 +56,7 @@ if __name__ == "__main__":
             "title": "Opening the black box of deep learning",
             "authors": ["Dian Lei", "Xiaoxiao Chen", "Jianfei Zhao"],
             "published": "2018-05-22T02:12:33Z",
-            "summary": "The great success of deep learning shows that its technology contains\nprofound truth, and understanding its internal mechanism not only has important\nimplications for the development of its technology and effective application in\nvarious fields, but also provides meaningful insights into the understanding of\nhuman brain mechanism. At present, most of the theoretical research on deep\nlearning is based on mathematics. This dissertation proposes that the neural\nnetwork of deep learning is a physical system, examines deep learning from\nthree different perspectives: microscopic, macroscopic, and physical world\nviews, answers multiple theoretical puzzles in deep learning by using physics\nprinciples. For example, from the perspective of quantum mechanics and\nstatistical physics, this dissertation presents the calculation methods for\nconvolution calculation, pooling, normalization, and Restricted Boltzmann\nMachine, as well as the selection of cost functions, explains why deep learning\nmust be deep, what characteristics are learned in deep learning, why\nConvolutional Neural Networks do not have to be trained layer by layer, and the\nlimitations of deep learning, etc., and proposes the theoretical direction and\nbasis for the further development of deep learning now and in the future. The\nbrilliance of physics flashes in deep learning, we try to establish the deep\nlearning technology based on the scientific theory of physics.",
+            "summary": "The great success of deep learning show that its technology contains\nprofound truth, and understanding its internal mechanism not only has important\nimplications for the development of its technology and effective application in\nvarious fields, but also provides meaningful insights into the understanding of\nhuman brain mechanism. At present, most of the theoretical research on deep\nlearning is based on mathematics. This dissertation proposes that the neural\nnetwork of deep learning is a physical system, examines deep learning from\nthree different perspectives: microscopic, macroscopic, and physical world\nviews, answers multiple theoretical puzzles in deep learning by using physics\nprinciples. For example, from the perspective of quantum mechanics and\nstatistical physics, this dissertation presents the calculation methods for\nconvolution calculation, pooling, normalization, and Restricted Boltzmann\nMachine, as well as the selection of cost functions, explains why deep learning\nmust be deep, what characteristics are learned in deep learning, why\nConvolutional Neural Networks do not have to be trained layer by layer, and the\nlimitations of deep learning, etc., and proposes the theoretical direction and\nbasis for the further development of deep learning now and in the future. The\nbrilliance of physics flashes in deep learning, we try to establish the deep\nlearning technology based on the scientific theory of physics.",
             "link": "http://arxiv.org/abs/1805.08355v1",
         }
     ]
--- src/saveToGraphDb.py
+++ src/saveToGraphDb.py
@@ -1,7 +1,16 @@
 from typing import List
 from neo4j import GraphDatabase
+from dotenv import load_dotenv
+import os
 
-driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "Arxiv123"))
+load_dotenv()
+
+NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
+NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password")
+
+# Create the driver
+driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
 
 
 def add_paper_to_db(
@@ -10,7 +19,6 @@ def add_paper_to_db(
     """
     Add a paper to the graph database.
     """
-    # TODO: move to .env
     query = """
     MERGE (p:Paper {paper_index: $paper_index})
     SET p.title = $title,
