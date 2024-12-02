f0ee6d3ab5a1686b1fccf5fde679cce5a8fe5848
= <=>
2024-12-02 20:36:10 +0100

    add function to add graph like data to neo4j

--- src/saveIdToGraphDb.py
+++ src/saveIdToGraphDb.py
@@ -1,7 +1,7 @@
 """
 This file contains functions that save paper ids and its references to a Neo4j graph database.
 """
-from typing import List
+from typing import List, Dict
 from neo4j import GraphDatabase
 from dotenv import load_dotenv
 import os
@@ -19,28 +19,42 @@ driver = GraphDatabase.driver(
     NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD)    
 )
 
-def add_paper_to_db(
-    paper_id: str, references: List[str]
-) -> None:
+def add_graph_to_db(graph: Dict[str, List[str]]) -> None:
     """
-    Add a paper to the graph database.
-
-    :param paper_id: The unique identifier of the paper.
-    :param references: The  ids that the paper references.
+    Write the whole graph to the database.
+    :param graph: The graph to write to the database.
     """
-    query = """
-    MERGE (p:Paper {paper_index: $paper_index})
-    SET p.references = $references
+    db_setup_query = "CREATE CONSTRAINT FOR (p:Paper) REQUIRE p.id IS UNIQUE"
+    add_paper_query = "MERGE (p:Paper {id: $paper_id})"
+    add_relationship_query = """
+        MATCH (p1:Paper {id: $paper_id}), (p2:Paper {id: $reference_id})
+        MERGE (p1)-[:REFERENCES]->(p2)
     """
 
     with driver.session(database=NEO4J_DATABASE) as session:
-        session.execute_write(
-            lambda tx: tx.run(
-                query,
-                paper_index=paper_id,
-                references=references,
+        # Ensure paper_id is unique
+        session.execute_write(lambda tx: tx.run(db_setup_query))
+
+        # Create a node for each paper
+        for paper_id in graph.keys():
+            session.execute_write(
+                lambda tx: tx.run(
+                    add_paper_query,
+                    paper_id=paper_id
+                )
             )
-        )
+        # Add the relationships
+        for paper_id, references in graph.items():
+            for reference_id in references:
+                session.execute_write(
+                    lambda tx: tx.run(
+                        add_relationship_query,
+                        paper_id=paper_id,
+                        reference_id=reference_id
+                    )
+                )
+    
+        
 
 def count_papers() -> int:
     """
@@ -60,7 +74,21 @@ def count_papers() -> int:
         
 
 if __name__ == "__main__":
-    # test_paper_id = "2301.00001"
-    # test_references = ["2201.00002", "2201.00003", "2201.00004"]
-    # add_paper_to_db(test_paper_id, test_references)
-    print(count_papers())
+    graph = {
+    '2405.18952v2': ['2403.17710v3', '2406.20060v1'],
+    '2403.17710v3': ['2408.17072v1'],
+    '2408.17072v1': ['2402.12366v1', '2403.08309v2', '2410.13785v1'],
+    '2410.04112v1': ['2407.09551v1'],
+    '2403.08309v2': ['2402.03746v3', '2402.10986v3'],
+    '2403.09032v2': ['2402.11907v2'],
+    '2402.12366v1': [],
+    '2402.10986v3': ['2410.12832v1', '2406.07295v2'],
+    '2410.13785v1': ['2402.03746v3', '2405.18952v2'],
+    '2406.20060v1': [],
+    '2402.03746v3': ['2406.20060v1'],
+    '2407.09551v1': ['2402.12366v1', '2408.17072v1'],
+    '2410.12832v1': ['2403.09032v2', '2405.18952v2', '2402.11907v2'],
+    '2402.11907v2': [],
+    '2406.07295v2': ['2403.17710v3']
+    }
+    add_graph_to_db(graph)
