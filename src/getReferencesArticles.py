816adac392a03d0b468539c9496ed76a16582ed1
koffipynb <hennecon.lucas@gmail.com>
2024-12-13 14:00:35 +0100

    Improved version of getReferencesArticles

--- src/getReferencesArticles.py
+++ src/getReferencesArticles.py
@@ -9,16 +9,25 @@ from typing import List
 # TODO: Improve the function to return other references than ArXiv references
 def extract_arxiv_references_from_article(text: str) -> List[str]:
     """
-    returns a list of ids corresponding the t
+    Extracts arXiv references in the formats 'arXiv:2001.08361' and 'abs/2001.08361'.
+    
+    :param text: The input text containing arXiv references.
+    :return: A list of extracted arXiv IDs (e.g., '2001.08361').
     """
-    references = re.findall(r"arXiv:\d{4}\.\d{4,5}", text)
-    references = [reference.split(":")[1] for reference in references]
+    #First version:
+    # references = re.findall(r"arXiv:\d{4}\.\d{4,5}", text)
+    # references = [reference.split(":")[1] for reference in references]
+
+    # Match both arXiv:XXXX.XXXX and abs/XXXX.XXXX formats
+    references = re.findall(r"(?:arXiv:|abs/)(\d{4}\.\d{4,5})", text)
+
+
     return references
 
 
 if __name__ == "__main__":
     from processPdf import extract_text_from_pdf
 
-    text = extract_text_from_pdf("http://arxiv.org/pdf/1805.08355v1")
+    text = extract_text_from_pdf("https://arxiv.org/pdf/1706.03762")
     references = extract_arxiv_references_from_article(text)
-    print(references)
+    print(references, len(references))
